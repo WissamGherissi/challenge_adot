81699d70505f6217b6f48e4f80e170655a7dd2e5
Wissam Gherissi <wissam.gherissi@polytechnique.edu>
2021-07-27 18:19:36 +0200

    modify requirements file add challenge_adot notebook add util.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # challenge_adot
+
+This project is for the technical challenge proposed by Adot to work on multilabel topic classification of URLs.
+
+It is composed of:
+#### challenge_adot.ipynb: a jupyter notebook that contains all the work established in order to study the topic classification problem
+#### requirements.txt:  a text file containing all modules required for the proper use of the code included in the project
+#### utils.py: a python file containing implemented functions that will be used in the jupyter notebook
+
+How to use:
+- All the work and evaluation is contained in the notebook, launch the notebook and install the required modules to successfully run the notebook and explore the work done during this challenge, with a few parameter initializations, there is multiple possibilities to try different models for fitting on the data
---
+++ utils.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+plt.style.use('ggplot')
+
+
+def plot_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+    with open(filepath, encoding='utf8') as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word]
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+
+    return embedding_matrix
+
+# def create_keras_pipeline():
+
